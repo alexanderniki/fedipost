dc2f6c498468f72979359a83448ecc4cb24d4a82
Alexander Niki <aalexanderniki@gmail.com>
2024-11-23 04:46:37 +0400

    Added fediverse Client model

---
+++ fedipost/models/client.py
@@ -0,0 +1,30 @@
+from http import client
+"""
+File: client.py
+Version: 0.1
+Author: Aleksandr Nikiforov (Lonin)
+Contact: https://alexanderniki.name
+Date: Nov 23, 2024
+Description: A fediverse client app/script
+"""
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Client:
+    """
+    Client
+
+    Attr:
+        api_token (str): Token
+        username (str): Username
+        login (str): Login
+        password (str): Password
+    """
+
+    api_token: str = ""
+    username: str = ""
+    login : str = ""
+    password = ""
