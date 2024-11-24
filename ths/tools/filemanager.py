a33c44871c08b3aadf3ce7b531a72ae968b606fa
Alexander Niki <aalexanderniki@gmail.com>
2024-11-24 13:05:32 +0400

    Added new methods into FileManager class

--- ths/tools/filemanager.py
+++ ths/tools/filemanager.py
@@ -21,11 +21,36 @@ class FileManager():
     def __init__(self) -> None:
         pass
 
-    def listdir(self, directory_path: str) -> List:
+    # FILES
+
+    def listdir(self, directory_path: str) -> List[str]:
+        """
+        Get a list of files in the directory
+
+        Args:
+            directory_path (str): Path to directory
+
+        Returns:
+            List[str]: A list of files in given directory
+        """
         ...
 
     def create_file(self, filename_path: str) -> None:
         ...
 
     def rename_file(self, filename_path: str, filename_path_new: str) -> str:
+        ...
+
+    # DIRECTORIES
+
+    def create_directory(self, directory_path: str) -> None:
+        ...
+
+    def rename_directory(self, directory_path: str, directory_path_new: str) -> str:
+        ...
+
+    def get_home_directory(self) -> str:
+        ...
+
+    def get_current_directory(self) -> str:
         ...
