f1577d90a355f655881c314b695e88da68d4a600
Alexander Niki <aalexanderniki@gmail.com>
2024-12-07 22:14:16 +0400

    Added scripts

---
+++ scripts/mastodon_randimg.py
@@ -0,0 +1,8 @@
+"""
+File: mastodon_randimg.py
+Version: 0.1
+Author: Aleksandr Nikiforov (Lonin)
+Contact: https://alexanderniki.name
+Date: Dec 7, 2024
+Description: Script for posting a random image from a directory to a Mastodon account
+"""
---
+++ scripts/mastodon_scheduled_text.py
@@ -0,0 +1,14 @@
+"""
+File: mastodonm_scheduled_text.py
+Version: 0.1
+Author: Aleksandr Nikiforov (Lonin)
+Contact: https://alexanderniki.name
+Date: Dec 07, 2024
+Description: Script for posting a simple text to a Mastodon account
+"""
+
+import time
+import schedule
+from mastodon import Mastodon
+
+
